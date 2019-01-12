a77aab8c166e533d3a5ab53083626eb69fccf279
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-12 04:58:52 +0000

    matching banned words

--- pset6/bleep/bleep.py
+++ pset6/bleep/bleep.py
@@ -16,10 +16,43 @@ def main():
     infile = argv[1]
     file_open = open(infile, "r", 1)
 
+    # Store list of banned words in set structure
+    banned_words = set(line.strip() for line in open(infile))
+    print(f"infile {infile} and this is the set {banned_words}")
+
     # Prompt user to provide a message
     message = get_string("What message would you like to censor? ")
 
+    tokens = set(message.split(' '))
+    print(f"{tokens}")
 
+    # Check to see if message words match any words from banned set
+    banned = banned_words.intersection(tokens)
+    # print(banned)
+
+    for elem in banned:
+        for i in elem:
+            print('*', end="")
+
+    print(' '.join(banned))
+
+    new_banned = ' '.join(banned)
+
+    for i in new_banned:
+        print('*', end="")
+
+    for i in new_banned:
+        print(message.replace(new_banned, '*'))
+    # if str(banned in message:
+    #     for c in banned:
+    #         print("*", end="")
+    # new_message = message.replace(for c in banned, "*")
+    # for c in banned:
+    #     print(c.replace(c, '*')
+
+
+    print(message.replace(str(banned), '*'))
 
 if __name__ == "__main__":
     main()
+
