a69b26a04f4b9854c9f50bd4b4f11b1bcac8da29
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-18 20:03:17 +0000

    Created a form in index.html and edited get_substrings in helpers.py

--- pset7/similarities/helpers.py
+++ pset7/similarities/helpers.py
@@ -2,14 +2,15 @@ from nltk.tokenize import sent_tokenize
 
 # sets are unordered collections with no duplicate elements
 
+
 def lines(a, b):
     """Return lines in both a and b"""
 
     # split each string into lines
     lines_a = set(a.splitlines())
-    print(lines_a)
+    # print(lines_a)
     lines_b = set(b.splitlines())
-    print(lines_b)
+    # print(lines_b)
 
     # return the list
     return lines_a & lines_b
@@ -20,8 +21,8 @@ def sentences(a, b):
 
     sentences_a = set(sent_tokenize(a, language='english'))
     sentences_b = set(sent_tokenize(b, language='english'))
-    print(sentences_a)
-    print(sentences_b)
+    # print(sentences_a)
+    # print(sentences_b)
 
     return sentences_a & sentences_b
 
@@ -31,20 +32,20 @@ def get_substrings(str, n):
     substrings = []
 
     # str[i:j] returns substring of s from index i to (not including) index j
-    for i in range(len(str)):
-        for j in range(n - i):
-            substrings.append((str[i:n + 1]))
-            return substrings
-            print(substrings)
+    for i in range(len(str) - n + 1):
+        substrings.append(str[i:i + n])
+        # print(substrings)
+
+    return substrings
 
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
     # split each string into all substrings of len n
-    substrings_a = set(get_substrings(a,n))
-    substrings_b = set(get_substrings(b,n))
-    print(substrings_a)
-    print(substrings_b)
+    substrings_a = set(get_substrings(a, n))
+    substrings_b = set(get_substrings(b, n))
+    # print(substrings_a)
+    # print(substrings_b)
 
     return substrings_a & substrings_b
