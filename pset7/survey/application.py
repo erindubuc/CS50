a6cd7d873a9b2ab80cc4e059fe3552b25492aef3
Erin Dubuc <erin.dubuc9@gmail.com>
2019-01-24 00:28:58 +0000

    Created web app for user to fill out/submit a form, and view all submissions

--- pset7/survey/application.py
+++ pset7/survey/application.py
@@ -9,6 +9,9 @@ app = Flask(__name__)
 # Reload templates when they are changed
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Students who have completed survey
+students = []
+
 
 @app.after_request
 def after_request(response):
@@ -31,9 +34,29 @@ def get_form():
 
 @app.route("/form", methods=["POST"])
 def post_form():
-    return render_template("error.html", message="TODO")
+    name = request.form.get("name")
+    email = request.form.get("email")
+    classTime = request.form.get("classTime")
+    language = request.form.get("language")
+
+    # If something was not entered into form
+    if not name or not email or not classTime or not language:
+        return render_template("error.html", message="Please be sure to enter your name and email, AND to choose a favorite langauge.")
+
+    # Upon success -> create sheet and redirect
+    file = open("survey.csv", "a")
+    writer = csv.writer(file)
+    writer.writerow((request.form.get("name"), request.form.get("email"),
+                     request.form.get("classTime"), request.form.get("language")))
+    file.close()
+    return redirect("/sheet")
 
 
 @app.route("/sheet", methods=["GET"])
 def get_sheet():
-    return render_template("error.html", message="TODO")
+
+    # Display submissions in HTML table
+    file = open("survey.csv", "r")
+    reader = csv.reader(file)
+    students = list(reader)
+    return render_template("results.html", students=students)
